a4f83574bd2e22791dd1654d34a1543ec524b546
Ivan Calapuja <icalapuja@gmail.com>
2021-06-26 08:45:57 -0500

    carga inicial

---
+++ src/main/java/pe/ivan/henkins/PruebaHenkinsApplication.java
@@ -0,0 +1,13 @@
+package pe.ivan.henkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaHenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaHenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/ivan/henkins/PruebaHenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ivan.henkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaHenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
